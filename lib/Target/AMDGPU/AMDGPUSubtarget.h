8cbe0ae0ccb99b7341735d802c44953679b8a38e
gregrodgers <grodgers@purdue.edu>
2017-04-05 15:17:31 -0500

    tread OS==CUDA like OS=amdhsa

--- lib/Target/AMDGPU/AMDGPUSubtarget.h
+++ lib/Target/AMDGPU/AMDGPUSubtarget.h
@@ -184,7 +184,8 @@ public:
   void ParseSubtargetFeatures(StringRef CPU, StringRef FS);
 
   bool isAmdHsaOS() const {
-    return TargetTriple.getOS() == Triple::AMDHSA;
+    return (TargetTriple.getOS() == Triple::AMDHSA) ||
+           (TargetTriple.getOS() == Triple::CUDA) ;
   }
 
   bool isMesa3DOS() const {
